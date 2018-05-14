6df335ece6e8e5489c73ffc49aa71ec274b11e8a
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-14 21:39:10 +0000

    promises

--- promises/cat_2_files.js
+++ promises/cat_2_files.js
@@ -3,13 +3,28 @@ var fsp = require('fs-promise');
 var file1promise = fsp.readFile('text1.txt');
 var file2promise = fsp.readFile('text2.txt');
 
-Promise.all([file1promise, file2promise])
-    .then(function(texts) {
-        return texts[0] + '\n\n' + texts[1];
-    })
-    .then(function(concatTexts) {
-        fsp.writeFile('concat.txt', concatTexts);
-    })
-    .catch(function(error) {
-        console.log("ERROR: ", error);
-    })
+var listOfFiles = [file1promise, file2promise];
+
+function concatNFiles(listOfFiles, outputFileName) {
+    var listOfFilePromises = [];
+    for (var i = 0, l = listOfFiles.length; i < l; i++) {
+        listOfFilePromises.push(fsp.readFile(listOfFiles[i]));
+    }
+    
+    Promise.all(listOfFilePromises)
+        .then(function(fileTexts) {
+            var concatText = "";
+            for (var i = 0, l = listOfFiles.length; i < l; i++) {
+                concatText += (fileTexts[i] + "/n/n");
+            }
+            return concatText;
+        })
+        .then(function(concatTexts) {
+            fsp.writeFile(outputFileName, concatTexts);
+        })
+        .catch(function(error) {
+            console.log("ERROR: ", error);
+        });
+}
+
+concatNFiles(listOfFiles, 'concat.txt');
---
+++ promises/cat_n_files.js
@@ -0,0 +1,15 @@
+var fsp = require('fs-promise');
+
+var file1promise = fsp.readFile('text1.txt');
+var file2promise = fsp.readFile('text2.txt');
+
+Promise.all([file1promise, file2promise])
+    .then(function(texts) {
+        return texts[0] + '\n\n' + texts[1];
+    })
+    .then(function(concatTexts) {
+        fsp.writeFile('concat.txt', concatTexts);
+    })
+    .catch(function(error) {
+        console.log("ERROR: ", error);
+    })
