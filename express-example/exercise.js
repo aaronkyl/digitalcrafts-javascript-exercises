e9b1db2a1e9582707892d74347334fe992ae6cd9
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-18 00:28:42 +0000

    exercises

--- express-example/exercise.js
+++ express-example/exercise.js
@@ -17,6 +17,19 @@ app.get('/cats_and_dogs', function(request, response) {
     response.send('Living together');
 });
 
+app.get('/greet/:name', function(request, response) {
+    var name = request.params.name;
+    response.send(`Hello, ${name}!`);
+});
+
+app.get('/year', function(request, response) {
+    var age = request.query.age;
+    var today = new Date();
+    var year = today.getFullYear();
+    var DOB = year - age;
+    response.send(`You were born in ${DOB}.`);
+});
+
 app.listen(8080, function() {
     console.log("Listening on port 8080");
 });
