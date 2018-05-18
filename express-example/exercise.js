8fd1ca53b964504e34f3eaf12a6078738d7151f0
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-18 00:18:27 +0000

    initial commit

---
+++ express-example/exercise.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello, world!');
+});
+
+app.get('/cats', function(request, response) {
+    response.send('Meow');
+});
+
+app.get('/dogs', function(request, response) {
+    response.send('Woof');
+});
+
+app.get('/cats_and_dogs', function(request, response) {
+    response.send('Living together');
+});
+
+app.listen(8080, function() {
+    console.log("Listening on port 8080");
+});
